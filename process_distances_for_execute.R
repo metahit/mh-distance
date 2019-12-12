aff6279d1ab28872b68501491d47cc2d4842a732
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2019-12-12 13:28:27 +0000

    wip

--- process_distances_for_execute.R
+++ process_distances_for_execute.R
@@ -476,13 +476,18 @@ for(scenario in scenarios){
       }
       # clear memory, remove nas, and save
       concat <- NULL
+      print(1)
       to_save[,tube_wkhr:=0]
       ## add tube travel for london residents
       if(city=='london') {
+        print(2)
         tube_travel <- do.call(rbind, lapply(one_city_las,function(i) synth_pops_scen[[i]][tube_wkhr>0,colnames(synth_pops_scen[[i]])%in%c('census_id','tube_wkhr'),with=F]) )
+        print(3)
         to_save <- merge(to_save,tube_travel,on='census_id',all=T)
+        print(4)
       }
       for(i in 2:ncol(to_save)) set(to_save,which(is.na(to_save[[i]])),i,0)
+      print(5)
       saveRDS(to_save,paste0('../mh-execute/inputs/distances/',scenario,city,'_inh_distances.Rds'))
       to_save <- c()
       #distance_for_inh[[city]] <- to_save
